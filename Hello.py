2b5ef4283bbfe68c5e7237078818d470bc158564
Mayuresh <Mnw2212@users.noreply.github.com>
2014-03-09 23:45:10 +0530

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,17 @@
+'''
+
+Hello Freshers
+
+This repository is only for you to learn github.
+
+Feel free to edit this file and others too...
+Adda few print statements, maybe a few clasess and functions too
+Upto you.
+
+Experiment and learn !
+
+'''
+
+print " Welcome "
+print " Change everything and anything you want "
+print " Make sure it runs "
